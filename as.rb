7c504008c2e6b94815933b4f6353f9f8713732f6
Isobe Kazuhiko <muramasa64@gmail.com>
2022-02-26 17:08:55 +0900

    implement '/authorize'

--- as.rb
+++ as.rb
@@ -1,14 +1,82 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/config_file'
+require 'uri'
+require 'logger'
 
-set :views, settings.root + '/views/as'
+class AuthorizationServer < Sinatra::Base
+    # config file
+    register Sinatra::ConfigFile
+    config_file './config.yaml'
 
-Client = Struct.new(:client_id, :client_secret, :redirect_uris, :scope)
-AuthServer = Struct.new(:authorization_endpoint, :token_endpoint)
+    # auto reload
+    register Sinatra::Reloader
 
-clients = [Client.new("oauth-client-1", "oauth-client-secret-1", ["http://localhost:4568/callback"], "foo bar")]
-auth_server = AuthServer.new("http://localhost:4567/authorize", "http://localhost:4567/token")
+    # sinatra settings
+    set :views, settings.root + '/views/as'
 
-get '/' do
-    erb :index, :locals => {:clients => clients, :auth_server => auth_server}
+    # logger
+    configure :production, :development do
+        enable :logging
+    end
+    logger = Logger.new(STDOUT)
+
+    # functions
+    def get_client(client_id)
+        settings.clients.find {|c| c['client_id'] == client_id}
+    end
+
+    def valid_redirect_uri?(client, redirect_uri)
+        return false unless redirect_uri
+        client['redirect_uris'].include?(redirect_uri)
+    end
+
+    def valid_scope(requested_scope, client_scope)
+        diff_scope = requested_scope - client_scope
+        !(diff_scope.size > 0)
+    end
+
+    def parse_scope(scopes)
+        return [] unless scopes
+        scopes.split(' ')
+    end
+
+    def generate_request_id
+        Random.urandom(8)
+    end
+
+    # routing
+    get '/' do
+        erb :index, :locals => {:clients => settings.clients, :auth_server => settings.auth_server}
+    end
+
+    get '/authorize' do
+        client = get_client(params['client_id'])
+        redirect_uri = params['redirect_uri'] ? params['redirect_uri'] : ''
+        logger.debug client
+
+        if !client
+            logger.info "Unknown client: #{client}"
+            erb :error, :locals => {:error => 'Unknown client'}
+        else
+            is_valid_redirect_uri = valid_redirect_uri?(client, redirect_uri)
+            if !is_valid_redirect_uri
+                logger.info "Mismatched redirect URI, expected #{client['redirect_uris']} got #{redirect_uri}"
+                erb :error, :locals => {:error => 'Invalid redirect URI'}
+            else
+                requested_scope = parse_scope(params['scope'])
+                client_scope = parse_scope(client['scope'])
+                is_valid_scope = valid_scope(requested_scope, client_scope)
+                unless is_valid_scope
+                    logger.info "Invalid scope"
+                    uri = URI.parse(params['redirect_uri'])
+                    uri.query = "error=invalid_scope"
+                    redirect uri
+                else
+                    request_id = generate_request_id
+                    erb :approve, :locals => {:client => client, :scope => requested_scope, :request_id => request_id}
+                end
+            end
+        end
+    end
 end
